b85e8cac37a1ef73712dcd78bf06f00914fd971b
hassan <hassansardar.info1@gmail.com>
2025-05-20 21:23:34 +0500

    update images

--- frontend/components/About/About.tsx
+++ frontend/components/About/About.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 import React from "react";
-// import MyComponent from "../components/MyComponent";
 import Marquee from "react-fast-marquee";
 import image1 from '@/public/images/B_K-removebg-preview.png'
 import image2 from '@/public/images/Black Illustrated School Logo.png'
 import image3 from '@/public/images/dashboard.png'
-import image4 from '@/public/images/grid.png'
+import image4 from '@/public/images/googlemeet.png'
 const App = () => (
   <Marquee>
-    <Image src={image1} alt="fds"/>
-    <Image src={image2} alt="fds"/>
-    <Image src={image3} alt="fds"/>
-    <Image src={image4} alt="fds"/>
-    <Image src={image2} alt="fds"/>
+    <Image src={image4} alt="fds" />
+    <Image src={image4} alt="fds" />
+    <Image src={image4} alt="fds" />
+    <Image src={image4} alt="fds" />
+    <Image src={image4} alt="fds" />
   </Marquee>
 );
 
